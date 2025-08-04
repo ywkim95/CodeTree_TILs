b1b8549e9d836700329b376d4a3f0a679e5eac5a
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-04 21:56:23 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 184ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250804/시험 통과 여부 확인하기 2/verify-test-passed-2.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const arr = fs.readFileSync(0).toString().trim().split("\n");
+const N = arr.shift();
+
+const doubleArr = arr.map((v) => v.split(" ").map(Number));
+
+const reduceArr = doubleArr.map((v) => {
+    return v.reduce((acc, cur) => acc + cur, 0) / 4;
+});
+
+let passedCount = 0;
+let str = "";
+for(const el of reduceArr) {
+    if(el > 60) {
+        str += "pass\n";
+        passedCount++;
+    } else {
+        str += "fail\n";
+    }
+}
+
+console.log(`${str}${passedCount}`);
