a2f40ed79d7407a668ad7f7f24a9d59aa0274cf4
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-05 22:09:44 +0900

    Title: 배열 놀이, Time: 189ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250805/배열 놀이/play-with-array.js
@@ -0,0 +1,30 @@
+const [nq, na, ...qa] = require('fs').readFileSync(0).toString().trim().split("\n");
+const [n,q] = nq.split(" ").map(Number);
+const nArr = na.split(" ").map(Number);
+const qArr = qa.map((v) => v.split(" ").map(Number));
+
+
+function func_a(a) {
+    return nArr[a - 1];
+};
+function func_b(b) {
+    return nArr.findIndex((n) => n === b) + 1;
+};
+function func_c(s, e) {
+    return nArr.slice(s-1, e).join(" ");
+};
+
+let str = "";
+const result = qArr.map((v) => {
+    switch(v[0]) {
+        case 1:
+            return func_a(v[1]);
+        case 2:
+            return func_b(v[1]);
+        case 3:
+            return func_c(v[1], v[2]);
+    }
+    return 0;
+}).filter((v) => v !== 0).join("\n");
+
+console.log(result);
