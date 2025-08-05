beeaea4dbfa88742007bbc9f4636832f602a96e7
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-05 22:36:15 +0900

    Title: 연속부분수열일까, Time: 164ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250805/연속부분수열일까/contiguous-array-or-not.js
+++ 250805/연속부분수열일까/contiguous-array-or-not.js
@@ -1,6 +1,31 @@
 const [n,a,b] = require('fs').readFileSync(0).toString().trim().split("\n");
 const [n1, n2] = n.split(" ").map(Number);
 
-console.log(a.includes(b) ? "Yes" : "No");
+function func() {
+    const matched = a.indexOf(b.at(0));
+    if(matched === -1) {
+        return "No";
+    }
+    // indexOf 사용 해보기
+    let match = 0;
+    let start = matched;
+    let idx = 0;
+    while(match !== n2) {
+        if(a.at(start+idx) === b.at(idx)) {
+            idx += 2;
+            match++;
+        } else {
+            idx = 0;
+            match = 0;
+            start += 2;
+        }
 
-// for(let i = 0; i < )
+        if(start >= a.length) {
+            break;
+        }
+    }
+
+    return match === n2 ? "Yes" : "No";
+}
+
+console.log(func());
