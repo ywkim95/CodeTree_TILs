5f7efa2cd678a4e35a2d4169540826e525b40d1f
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-06 23:25:39 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 166ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250806/중복되지 않는 정수 중 최대/max-of-unique-number.js
+++ 250806/중복되지 않는 정수 중 최대/max-of-unique-number.js
@@ -1,22 +1,17 @@
 const [n, arr] = require('fs').readFileSync(0).toString().trim().split("\n");
 const num = Number(n);
 const numArr = arr.split(" ").map(Number);
-
 let maxValue = Number.MIN_SAFE_INTEGER;
-let doubleCheck = 0;
-for(let i = 0; i < num; ++i) {
-    if(numArr[i] === maxValue) {
-        doubleCheck = 0;
-        maxValue = -1;
-        continue;
-    }
 
-    if(numArr[i] > maxValue) {
-        maxValue = numArr[i];
-        doubleCheck = 1;
-        continue;
-    }
+// 그러면 내가 한게 다른 방식이니까 지금 생각을 해보면 for문이 아니라 find로 값을 찾고 갯수를 계속 비교하면서 진행을 해야하는걸까? 
+// 그러면 map으로 피벗을 진행한다음 갯수를 체크하면 좋을듯 ㅎ
 
-}
+const map = new Map();
 
-console.log(maxValue);
+numArr.forEach((v) => {
+    map.set(v, (map.get(v) || 0) + 1);
+});
+
+const result = Array.from(map, ([name, value]) => ({name, value})).filter((v) => v.value < 2).sort((a,b) => a.name > b.name);
+
+console.log(result[0]? result[0].name : -1);
