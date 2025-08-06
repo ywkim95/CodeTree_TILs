de1c12ae06750822f661da4cf85d20246dfdc5c8
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-07 00:18:06 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 171ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250807/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.js
@@ -0,0 +1,17 @@
+const [n, ar] = require("fs").readFileSync(0).toString().trim().split("\n");
+const num = Number(n);
+const arr = ar.split(" ").map(Number);
+
+let diff = 0;
+let idx = 0;
+while(idx !== num - 1) {
+    const el = arr[idx];
+    const res = arr.slice(idx+1).filter((v) => v > el).reduce((acc, cur) => (acc - el) > (cur - el) ? acc-el : cur-el, diff);
+    if(res > diff) {
+        diff = res;
+    }
+
+    idx++;
+}
+
+console.log(diff)
