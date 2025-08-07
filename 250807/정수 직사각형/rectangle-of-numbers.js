ddbe6fbd544e47b806b1d9f4eb073df212d8ab70
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-07 14:37:35 +0900

    Title: 정수 직사각형, Time: 173ms, Memory: 15MB, Status: Passed - Codetree

--- 250807/정수 직사각형/rectangle-of-numbers.js
+++ 250807/정수 직사각형/rectangle-of-numbers.js
@@ -1,7 +1,7 @@
 const [n, m] = require('fs').readFileSync(0).toString().trim().split(" ").map(Number);
 
 const arr = Array(n).fill(0).map((_, i) => {
-    return Array(m).fill(0).map((__, j) => (i*(n-1))+j+1).join(" ");
+    return Array(m).fill(0).map((__, j) => (i*(m))+j+1).join(" ");
 }).join("\n");
 
 
