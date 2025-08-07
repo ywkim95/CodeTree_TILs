4b7deb7b95d5b3b848fa1a40ad15f81b77c75878
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-07 15:02:53 +0900

    Title: 아래로 사각형 채우기, Time: 178ms, Memory: 15MB, Status: Passed - Codetree

--- 250807/아래로 사각형 채우기/filling-rectangle-with-downflow.js
+++ 250807/아래로 사각형 채우기/filling-rectangle-with-downflow.js
@@ -1,14 +1,13 @@
-const input = require('fs').readFileSync(0).toString().trim().split("\n");
-const doubleArr = input.map((v) => v.split(" ").map(Number));
+const input = require('fs').readFileSync(0).toString().trim();
+const n = Number(input);
 
-const result = Array(doubleArr.length).fill(0).map((v) => Array(doubleArr[0].length).fill(0));
+const result = Array(n).fill(0).map((v) => Array(n).fill(0));
 
 let num = 1;
-for(let i = 0; i < doubleArr.length; i++) {
-    for(let j = 0; j < doubleArr[0].length; j++) {
+for(let i = 0; i < n; i++) {
+    for(let j = 0; j < n; j++) {
         result[j][i] = num++;
     }
 }
-
 console.log(result.map((v) => v.join(" ")).join("\n"));
 
