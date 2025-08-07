40db4d85ff7c7844981b6169fa90c16832443b26
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-07 13:38:20 +0900

    Title: 배열의 평균, Time: 177ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250807/배열의 평균/ave-of-array.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const ar = fs.readFileSync(0).toString().trim().split("\n");
+
+const doubleArr = ar.map((v) => v.split(" ").map(Number));
+
+const row = doubleArr
+    .map((arr) => arr.reduce((acc, cur) => acc + (+cur),0) / 4)
+    .reduce((acc, cur) => `${acc} ${cur.toFixed(1)}`,"")
+    .trim();
+const arr = [];
+for(let i = 0; i < doubleArr[0].length; i++) {
+    const sum = doubleArr[0][i] + doubleArr[1][i];
+    arr.push(sum/2);
+}
+
+const col = arr.reduce((acc, cur) => `${acc} ${cur.toFixed(1)}`,0).trim();
+
+const avg = doubleArr.flatMap(x=>x).reduce((acc, cur) => acc + cur, 0)/8;
+
+console.log(`${row}\n${col}\n${avg}`);
