56cc6fcd2f6d7a26003b936f3316a050c9adb2d6
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-07 14:55:17 +0900

    Title: 두 개의 격자 비교하기, Time: 174ms, Memory: 15MB, Status: Passed - Codetree

--- 250807/두 개의 격자 비교하기/compare-two-grid.js
+++ 250807/두 개의 격자 비교하기/compare-two-grid.js
@@ -1,8 +1,7 @@
 const [nm, ...arr] = require("fs").readFileSync(0).toString().trim().split("\n");
 const [n,m] = nm.split(" ").map(Number);
-const first = arr.slice(0, n).map((v) => v.split(" ").map(Number));
-const second = arr.slice(n).map((v) => v.split(" ").map(Number));
-
+const first = arr.slice(0, n).map((v) => v.trim().split(" ").map(Number));
+const second = arr.slice(n).map((v) => v.trim().split(" ").map(Number));
 for(let i = 0; i < n; i++) {
     for(let j = 0; j < m; j++) {
         if(first[i][j] === second[i][j]) {
