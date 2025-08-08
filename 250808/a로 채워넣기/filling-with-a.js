a8b28e9276a603021274580dc29d3e0d81a2cc87
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-08 23:23:30 +0900

    Title: a로 채워넣기, Time: 176ms, Memory: 15MB, Status: Passed - Codetree

--- 250808/a로 채워넣기/filling-with-a.js
+++ 250808/a로 채워넣기/filling-with-a.js
@@ -1,4 +1,14 @@
-let str = require('fs').readFileSync(0).toString().trim();
+const str = require('fs').readFileSync(0).toString().trim();
 
-str = str.slice(0,1) + 'a' + str.slice(2,-2) + 'a' + str.slice(-1);
-console.log(str);
+let newStr = '';
+
+for(let i = 0; i < str.length; i++) {
+    if(i === 1 || i === str.length - 2) {
+        newStr += 'a';
+    } else {
+        newStr += str.at(i)
+    }
+}
+
+
+console.log(newStr)
