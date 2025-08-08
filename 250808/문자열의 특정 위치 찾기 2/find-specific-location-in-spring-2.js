600d2f92f24a51ee9e3d7b004d150045b07b4001
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-08 21:17:01 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 167ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250808/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.js
@@ -0,0 +1,14 @@
+const input = require('fs').readFileSync(0).toString().trim();
+
+const arr = ['apple','banana','grape','blueberry','orange'];
+const result = [];
+
+for(const el of arr) {
+    if(el.at(2) === input || el.at(3) === input) {
+        result.push(el);
+    }
+}
+
+const str = result.map((v) => `${v}\n`);
+
+console.log(`${str}${result.length}`);
