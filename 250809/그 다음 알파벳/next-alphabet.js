beb7d7c63405b7dcf79b95b53ce4e7e8708dd0b9
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-09 12:28:44 +0900

    Title: 그 다음 알파벳, Time: 175ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250809/그 다음 알파벳/next-alphabet.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync(0).toString().trim();
+
+
+const char = input === 'z' ? input.charCodeAt(0) - 25 : input.charCodeAt(0) + 1;
+const result = String.fromCharCode(char);
+
+console.log(result);
