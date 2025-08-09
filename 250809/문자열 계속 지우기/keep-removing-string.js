01328435f54ff78d5fa556628bd9a820d538cf65
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-09 10:49:02 +0900

    Title: 문자열 계속 지우기, Time: 182ms, Memory: 15MB, Status: Passed - Codetree

--- 250809/문자열 계속 지우기/keep-removing-string.js
+++ 250809/문자열 계속 지우기/keep-removing-string.js
@@ -2,7 +2,7 @@ let [a, b] = require("fs").readFileSync(0).toString().trim().split("\n");
 
 while(a.includes(b)) {
     const idx = a.indexOf(b);
-    a = a.slice(0, idx) + a.slice(idx+2);
+    a = a.slice(0, idx) + a.slice(idx+b.length);
     
 }
 
