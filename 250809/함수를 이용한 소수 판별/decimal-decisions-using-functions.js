c224a0c52375b5ee2f9376e693105811556b35a6
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-09 19:09:26 +0900

    Title: 함수를 이용한 소수 판별, Time: 169ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250809/함수를 이용한 소수 판별/decimal-decisions-using-functions.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [A, B] = input[0].split(" ").map(Number);
+
+// Please Write your code here.
+
+function countPrimeNumbers(n,m) {
+    let sum = 0;
+    for(let i = n; i <= m; ++i) {
+        if(i === 2 || i === 3 || i === 5 || i === 7) {
+            sum += i;
+            continue;
+        }
+        if(i%2 === 0 || i%3 === 0 || i%5===0 || i%7===0) {
+            continue;
+        }
+        sum += i;
+    }
+
+    return sum;
+}
+
+console.log(countPrimeNumbers(A,B))
