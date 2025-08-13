210d46c9a6663a7607ab97207b8453d8431f87b8
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-13 20:40:16 +0900

    Title: Top K 숫자 구하기, Time: 175ms, Memory: 15MB, Status: Passed - Codetree

--- 250813/Top K 숫자 구하기/kth-number.js
+++ 250813/Top K 숫자 구하기/kth-number.js
@@ -4,5 +4,5 @@ const [n, k] = input[0].split(' ').map(Number);
 const nums = input[1].trim().split(' ').map(Number);
 
 // Please Write your code here.
-nums.sort();
+nums.sort((a,b) => a - b);
 console.log(nums[k-1]);
