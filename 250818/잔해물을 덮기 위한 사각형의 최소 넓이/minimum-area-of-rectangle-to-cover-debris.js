ca7e329e5b3823c342023aa548d7235880cdc8c5
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-18 22:11:48 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 332ms, Memory: 62MB, Status: Passed - Codetree

--- 250818/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
+++ 250818/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
@@ -6,15 +6,13 @@ const rect2 = input[1].split(' ').map(Number);
 
 
 // Please Write your code here.
-const coordinates = new Map();
+const coordinates = Array.from({length:2001}, () => Array(2001).fill(0));
 
 function drawRect(rect, value = 1) {
     const [x1, y1, x2, y2] = rect;
     for(let i = x1; i < x2; i++) {
         for(let j = y1; j < y2; j++) {
-            const key = `${i} ${j}`;
-            const cur = coordinates.get(key) || 0;
-            coordinates.set(key, cur + value);
+            coordinates[i+1000][j+1000] = value;
         }
     }
 }
@@ -23,22 +21,21 @@ drawRect(rect1);
 drawRect(rect2, -1);
 let minX = Number.MAX_SAFE_INTEGER, minY = Number.MAX_SAFE_INTEGER;
 let maxX = Number.MIN_SAFE_INTEGER, maxY = Number.MIN_SAFE_INTEGER;
-let flag = false;
-for(const [key, value] of coordinates) {
-    if(value > 0) {
-        flag = true;
-        const [x,y] = key.split(" ").map(Number);
-        if(x > maxX) maxX = x;
-        if(x < minX) minX = x;
-        if(y > maxY) maxY = y;
-        if(y < minY) minY = y;
-    }
-}
+const filteredList = coordinates.filter((coord) => coord.some((v) => v > 0));
 
-
-if(!flag) {
+if(filteredList.length === 0) {
     console.log(0);
 } else {
+    for(let i = 0; i < filteredList.length; ++i) {
+        for(let j = 0; j < filteredList[i].length; ++j) {
+            if(filteredList[i][j] > 0) {
+                if(i-1000 > maxX) maxX = i-1000;
+                if(i-1000 < minX) minX = i-1000;
+                if(j-1000 > maxY) maxY = j-1000;
+                if(j-1000 < minY) minY = j-1000;
+            }
+        }
+    }
     const size = (maxX - minX + 1) * (maxY - minY + 1);
     console.log(size);
 }
