f54d307a89bfc6f95a05c6c9c6e59be5d527de8c
KimYoungWoo <rladuddn3924@gmail.com>
2025-08-21 21:28:54 +0900

    Title: 별표 출력하기 7, Time: 165ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250821/별표 출력하기 7/print-star-7.js
@@ -0,0 +1,13 @@
+const input = require('fs').readFileSync(0).toString().trim();
+const n = Number(input);
+let str = '';
+for(let i = 1; i <= n; i++) {
+    const temp = Array(i).fill('*').join(" ");
+    str += temp;
+
+    if(i !== n) {
+        str += '\n';
+    }
+}
+
+console.log(str);
